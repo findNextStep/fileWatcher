7b9bd822a97e951ccd8818cba97af305df437ff1
findNextStep <findNextStep@gmail.com>
2018-12-13 14:51:04 +0800

    [feat]添加对vim一类使用覆盖写入的编辑器修改检测的支持

--- src/fileWatcher.cpp
+++ src/fileWatcher.cpp
@@ -7,18 +7,18 @@ namespace fileWatcher {
 
 fileWatcher::fileWatcher(const std::string &fileName,
                          const std::function<void(void)> &call_back):
-    inotify_fd(inotify_init()), file_name(fileName),need_watching(true) {
+    inotify_fd(inotify_init()), file_name(fileName), need_watching(true) {
     this->call_back_func = call_back;
 }
 
-fileWatcher::~fileWatcher(){
+fileWatcher::~fileWatcher() {
     need_watching = false;
     startWatch();
     stopWatch();
 }
 
 bool fileWatcher::startWatch() {
-    wd = inotify_add_watch(inotify_fd, file_name.c_str(), IN_CLOSE_WRITE);
+    wd = inotify_add_watch(inotify_fd, file_name.c_str(), IN_CLOSE_WRITE | IN_ATTRIB);
     return true;
 }
 
@@ -31,7 +31,6 @@ bool fileWatcher::watchOnce() {
     char *a = new char[(10 * (sizeof(struct inotify_event) + file_name.size() + 1))];
     auto readnum = read(inotify_fd, a, 100);
     if(readnum > 0) {
-        std::string str(a, readnum);
         for(auto c = a; c < a + readnum;) {
             auto event = (struct inotify_event *) c;
             {
@@ -39,19 +38,26 @@ bool fileWatcher::watchOnce() {
                     if(event->mask & IN_CLOSE_WRITE) {
                         delete a;
                         return true;
+                    } else if(event->mask & IN_ATTRIB) {
+                        // 一些编辑器(比如vim)使用文件覆盖的方法
+                        // 写入文件，此时需要重新启动一次读
+                        stopWatch();
+                        startWatch();
+                        delete a;
+                        return true;
                     }
                 }
             }
             c += sizeof(struct inotify_event) + event->len;
         }
-    } 
+    }
     delete a;
     return false;
 }
 
 void fileWatcher::run() {
     while(need_watching) {
-        if  (watchOnce()){
+        if(watchOnce()) {
             this->call_back_func();
         }
     }
