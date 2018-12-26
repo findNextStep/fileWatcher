4d8e9bfcc30dd9ab4ffa0e2e8a00636e2aff8be3
findNextStep <findNextStep@gmail.com>
2018-12-26 13:20:54 +0800

    [feat]添加可执行程序监视器

---
+++ include/exeWatcher.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <string>
+#include <functional>
+
+namespace wmj {
+namespace fileWatcher {
+class exeWatcher {
+public:
+    /**
+     * @brief exeWatcher 文件监视类的构造函数
+     *
+     * @param fileName 被监视的文件名称
+     * @param call_back 发现文件更改后的回调函数
+     */
+    exeWatcher(const std::string &fileName, const std::function<void(void)> &call_back);
+    ~exeWatcher();
+
+    /**
+     * @brief startWatch 开始监视
+     *
+     * @return 是否调用成功
+     */
+    bool startWatch();
+
+    /**
+     * @brief stopWatch 停止监视
+     *
+     * @return 是否调用成功
+     */
+    bool stopWatch();
+
+    /**
+     * @brief watchOnce 进行一次文件监视
+     * 如果监视过程中发生异常，例如监视被另一个线程终止
+     * 会返回false
+     * @return 文件是否发生修改
+     */
+    bool watchOnce();
+    /**
+     * @brief run 执行监视循环
+     * 执行后阻塞线程，如果发生被监视文件修改，调用回调函数
+     */
+    void run();
+
+private:
+    const int inotify_fd;
+    const std::string file_name;
+    bool need_watching;
+    int wd;
+    std::function<void(void)>call_back_func;
+};
+} // fileWatcher
+} // wmj
---
+++ src/exeWatcher.cpp
@@ -0,0 +1,54 @@
+#include <exeWatcher.hpp>
+#include <sys/inotify.h>
+#include <unistd.h>
+
+namespace wmj {
+namespace fileWatcher {
+exeWatcher::exeWatcher(const std::string &fileName,
+                       const std::function<void(void)> &call_back):
+    inotify_fd(inotify_init()), file_name(fileName), need_watching(true) {
+    this->call_back_func = call_back;
+}
+
+exeWatcher::~exeWatcher() {
+    need_watching = false;
+    startWatch();
+    stopWatch();
+}
+bool exeWatcher::startWatch() {
+    wd = inotify_add_watch(inotify_fd, file_name.c_str(),
+                           IN_CLOSE | IN_OPEN);
+    return true;
+}
+
+bool exeWatcher::stopWatch() {
+    inotify_rm_watch(this->inotify_fd, this->wd);
+    return true;
+}
+
+bool exeWatcher::watchOnce() {
+    char *a = new char[(10 * (sizeof(struct inotify_event) + file_name.size() + 1))];
+    auto readnum = read(inotify_fd, a, 100);
+    if(readnum > 0) {
+        unsigned int mask  = 0;
+        for(auto c = a; c < a + readnum;) {
+            auto event = (struct inotify_event *) c;
+            if(event->wd == this->wd) {
+                mask |= event->mask;
+            }
+            c += sizeof(struct inotify_event) + event->len;
+        }
+    }
+    delete a;
+    return false;
+}
+void exeWatcher::run() {
+    while(need_watching) {
+        if(watchOnce()) {
+            this->call_back_func();
+        }
+    }
+
+}
+} // fileWatcher
+} // wmj
