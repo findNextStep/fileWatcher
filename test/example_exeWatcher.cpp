c42dc53a80a5e92ebb084dec3600af8ff8b3f18b
findNextStep <findNextStep@gmail.com>
2018-12-27 11:07:47 +0800

    [test]根据api更改更新样例

--- test/example_exeWatcher.cpp
+++ test/example_exeWatcher.cpp
@@ -8,14 +8,14 @@ int main(int argc, char **argv) {
     } else {
         return 1;
     }
-    wmj::fileWatcher::exeWatcher ew(file_name, []() {});
-    ew.startWatch();
-    while(1) {
-        if(ew.watchOnce()) {
-            std::cout << "true" << std::endl;
+    wmj::fileWatcher::exeWatcher ew(file_name, [file_name](bool if_open) {
+        if(if_open) {
+            std::cout << file_name << " launched" << std::endl;
         } else {
-            std::cout << "false" << std::endl;
+            std::cout << file_name << " close" << std::endl;
         }
-    }
+    });
+    ew.startWatch();
+    ew.run();
     return 0;
 }
