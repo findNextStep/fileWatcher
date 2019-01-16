76f00e2a50afe58444dcce33c57561ae8a7410cb
findNextStep <findNextStep@gmail.com>
2019-01-16 14:38:47 +0800

    构建一些测试用例

--- test/test_case/test_fileWatcher.cpp
+++ test/test_case/test_fileWatcher.cpp
@@ -16,14 +16,11 @@ go_bandit([]() {
     }
     bandit::describe("写入测试", [file_name]() {
         bool has_change = false;
-        // auto exe_ptr = std::make_unique<wmj::fileWatcher::fileWatcher>(file_name, [&has_change]() {
-        //     has_change = true;
-        // });
-        wmj::fileWatcher::fileWatcher *exe_ptr = new wmj::fileWatcher::fileWatcher(file_name, [&has_change]() {
+        auto exe_ptr = std::make_unique<wmj::fileWatcher::fileWatcher>(file_name, [&has_change]() {
             has_change = true;
         });
-        std::thread run_thread([&]() {
-            exe_ptr->startWatch();
+        exe_ptr->startWatch();
+        std::thread run_thread([&exe_ptr]() {
             exe_ptr->run();
         });
         bandit::before_each([&]() {
@@ -40,9 +37,20 @@ go_bandit([]() {
             std::ofstream of(file_name, std::ios::app);
             of << "some txt" << std::endl;
         });
-        // exe_ptr.reset(nullptr);
-        delete exe_ptr;
-        run_thread.join();
+        bandit::it("常规写入", [&]() {
+            std::ofstream of(file_name);
+            of << "some txt" << std::endl;
+        });
+        bandit::it("mv覆盖", [&]() {
+            const std::string swp_file_name = file_name + ".swp";
+            std::ofstream of(swp_file_name);
+            of << "some txt" << std::endl;
+            system(("mv " + swp_file_name + " " + file_name).c_str());
+        });
+        exe_ptr.reset();
+        if(run_thread.joinable()) {
+            run_thread.join();
+        }
     });
 });
 
